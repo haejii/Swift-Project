bac84f00a3c5537b6c65a1f047155d23fa93c474
haejii <yanghagi@naver.com>
2022-01-08 14:00:37 +0900

    Connect outlet and button

--- Desktop/Project1_Swift/weatherApp/weatherApp/ViewController.swift
+++ Desktop/Project1_Swift/weatherApp/weatherApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+    }
+    
 }
 
