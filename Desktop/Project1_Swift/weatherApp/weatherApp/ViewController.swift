57be461ff541f195f20d46d124d92b683b16448a
haejii <yanghagi@naver.com>
2022-01-08 13:52:20 +0900

    Implement Basic UI

---
+++ Desktop/Project1_Swift/weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by 양혜지 on 2022/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
