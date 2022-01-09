6c5be5570ff50e61ab941fff2cbe0991a32cc3ee
haejii <yanghagi@naver.com>
2022-01-09 22:14:37 +0900

    Finish Weather App:

--- Desktop/Project1_Swift/weatherApp/weatherApp/WeatherInformation.swift
+++ Desktop/Project1_Swift/weatherApp/weatherApp/WeatherInformation.swift
@@ -16,36 +16,38 @@ import Foundation
  */
 
 struct WeatherInformation: Codable {
-    let weather: [Weather]
-    let temp: Temp
-    let name : String
-    
-    enum Codingkeys: String, CodingKey {
-        case weather
-        case temp = "main"
-        case name
-    }
+  let weather: [Weather]
+  let temp: Temp
+  let name: String
+
+  enum CodingKeys: String, CodingKey {
+    case weather
+    case temp = "main"
+    case name
+  }
 }
 
+
 struct Weather: Codable {
-    let id: Int
-    let main: String
-    let description: String
-    let icon: String
+  let id: Int
+  let main: String
+  let description: String
+  let icon: String
 }
+
 
 // JSON과 이름 다르게 하고 프로퍼티 이름이 달라도 매핑 시키기
 struct Temp: Codable {
-    let temp: Double
-    let feelsLike: Double
-    let minTemp: Double
-    let maxTemp: Double
+  let temp: Double
+  let feelsLike: Double
+  let minTemp: Double
+  let maxTemp: Double
     
     // 매핑시키기
-    enum Codingkeys: String, CodingKey {
-        case temp
-        case feelsLike = "feels_like"
-        case minTemp = "temp_min"
-        case maxTemp = "temp_max"
+    enum CodingKeys: String, CodingKey {
+      case temp
+      case feelsLike = "feels_like"
+      case minTemp = "temp_min"
+      case maxTemp = "temp_max"
     }
-}
+  }
