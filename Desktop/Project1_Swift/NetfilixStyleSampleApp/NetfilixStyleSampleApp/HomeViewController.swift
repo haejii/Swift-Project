2a5110cf02d6fb9663e064272600f7e389024c0d
haejii <yanghagi@naver.com>
2022-02-20 23:00:06 +0900

    Setting Navigation Item

--- Desktop/Project1_Swift/NetfilixStyleSampleApp/NetfilixStyleSampleApp/HomeViewController.swift
+++ Desktop/Project1_Swift/NetfilixStyleSampleApp/NetfilixStyleSampleApp/HomeViewController.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 class HomeViewController: UICollectionViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 네비게이션 설정
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        // 네비게이션 버튼
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "netfiix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+        
+   
+    }
 }
