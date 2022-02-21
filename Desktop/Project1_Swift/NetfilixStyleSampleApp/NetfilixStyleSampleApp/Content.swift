39191b428002630bea7a67458be02805cd6c14f3
haejii <yanghagi@naver.com>
2022-02-21 21:02:32 +0900

    Make Decodable Struct

---
+++ Desktop/Project1_Swift/NetfilixStyleSampleApp/NetfilixStyleSampleApp/Content.swift
@@ -0,0 +1,30 @@
+//
+//  Content.swift
+//  NetfilixStyleSampleApp
+//
+//  Created by 양혜지 on 2022/02/21.
+//
+
+import UIKit
+
+struct Content: Decodable {
+    let sectionType: String
+    let sectionName: String
+    let contentItem: [Item]
+    
+    enum SectionType: String, Decodable {
+        case basic
+        case main
+        case large
+        case rank
+    }
+}
+
+struct Item: Decodable {
+    let description: String
+    let imageName: String
+    
+    var image: UIImage {
+        return UIImage(named: imageName) ?? UIImage()
+    }
+}
--- Desktop/Project1_Swift/NetfilixStyleSampleApp/NetfilixStyleSampleApp/HomeViewController.swift
+++ Desktop/Project1_Swift/NetfilixStyleSampleApp/NetfilixStyleSampleApp/HomeViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class HomeViewController: UICollectionViewController {
+    
+    var contents: [Content] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,8 +22,35 @@ class HomeViewController: UICollectionViewController {
         
         // 네비게이션 버튼
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "netfiix_icon"), style: .plain, target: nil, action: nil)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
         
+        //Data 설정, 가져오기
+        contents = getContents()
    
     }
+    
+    func getContents() -> [Content] {
+        guard let path = Bundle.main.path(forResource: "Content", ofType: "plist"),
+              let data = FileManager.default.contents(atPath: path),
+              let list = try? PropertyListDecoder().decode([Content].self, from: data) else { return []}
+        return list
+    }
+}
+
+
+// UICollctionView DataSource, Delegate
+extension HomeViewController {
+    //섹션당 보여질 셀의 개수
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return contents[section].contentItem.count
+        }
+    }
+    
+    //콜렉션뷰 셀 설정
+ 
+    
 }
